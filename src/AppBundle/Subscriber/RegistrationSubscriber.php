16c4dd895976d311e98e4fa00cb4005fefdb89c6
Axel Venet <kern046@gmail.com>
2017-11-14 00:36:26 +0100

    Implement registration subscriber to redirect properly the user after successful registration

---
+++ src/AppBundle/Subscriber/RegistrationSubscriber.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Subscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+use FOS\UserBundle\FOSUserEvents;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+use FOS\UserBundle\Event\FormEvent;
+
+use Psr\Log\LoggerInterface;
+
+class RegistrationSubscriber implements EventSubscriberInterface
+{
+    /** @var UrlGeneratorInterface **/
+    protected $router;
+
+    /**
+     * @param UrlGeneratorInterface $router
+     */
+    public function __construct(UrlGeneratorInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FOSUserEvents::REGISTRATION_SUCCESS => 'onRegistrationSuccess'
+        );
+    }
+
+    /**
+     * @param FormEvent $event
+     */
+    public function onRegistrationSuccess(FormEvent $event)
+    {
+        $event->setResponse(new RedirectResponse($this->router->generate('homepage')));
+    }
+}
