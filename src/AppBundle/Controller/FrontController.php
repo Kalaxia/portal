67fdd96133d59c64fe2336b9e5e3a9bc474c7cb4
Axel Venet <kern046@gmail.com>
2017-11-23 15:19:44 +0100

    Implement basic dashboard view

--- src/AppBundle/Controller/FrontController.php
+++ src/AppBundle/Controller/FrontController.php
@@ -3,6 +3,10 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+use AppBundle\Manager\Game\ServerManager;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -16,6 +20,17 @@ class FrontController extends Controller
         // replace this example code with whatever you need
         return $this->render('front/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+    
+    /**
+     * @Security("has_role('ROLE_USER')")
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function dashboardAction(Request $request)
+    {
+        return $this->render('front/dashboard.html.twig', [
+            'servers' => $this->get(ServerManager::class)->getOpenedServers()
         ]);
     }
 }
