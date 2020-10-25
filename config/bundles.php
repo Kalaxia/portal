d9e124bc6bb97c7666cfd6c7317e249667ec4d87
Axel <avenet@wamiz.com>
2020-10-25 11:54:46 +0100

    Require UID component and Maker bundle

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
